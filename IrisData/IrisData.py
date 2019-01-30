42dcaf43d38b849535c2c866d38afe458697c659
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-01-30 10:45:42 +0800

    visualization

--- IrisData/IrisData.py
+++ IrisData/IrisData.py
@@ -1,2 +1,33 @@
+import matplotlib.pyplot as plt
 
-print("hello world")
+
+dataNames = ["sepal length","sepal width","petal length","petal width","class"]
+classNames ={"Iris-setosa":'k',"Iris-versicolor":'b',"Iris-virginica":'r'}
+
+with open("iris.data.txt",'r') as f:
+    data=f.readlines()
+for i in range(len(data)):
+    data[i] = data[i].strip().split(',')
+flowers = []
+
+for row in data:
+    flower = {}
+    for att in range(len(dataNames)):
+        try:
+            flower[dataNames[att]] = float(row[att])
+        except:
+            flower[dataNames[att]] = str(row[att])
+    flowers.append(flower)
+
+for i in range(4):
+    for ii in range(4):
+        if(ii>i):
+            for flower in flowers:
+                
+                x=(flower[dataNames[i]])
+                y=(flower[dataNames[ii]])
+                color = classNames[flower[dataNames[4]]]
+                plt.scatter(x,y,c=color)
+            plt.xlabel(dataNames[i])
+            plt.ylabel(dataNames[ii])
+            plt.show()
